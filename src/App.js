3af8fc16fc7e511271d95fc64ebd31e24b35b336
dwdjjj <dwdjjj@naver.com>
2021-11-25 15:23:50 +0900

    CSS module을 사용할 경우 classNames.bind사용하여 편리하게 처리하는 실습

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import "./App.css";
+// import "./App.scss";
+import styles from "./App.module.css";
+import Button from "./components/Button";
+
+// console.log(styles);
+// {
+//   App: "App_App__16ZpL"
+//   App-logo-spin: "App_App-logo-spin__1e7sv"
+//   header: "App_header__3ZZ1n"
+//   link: "App_link__TB6Cl"
+//   logo: "App_logo__1ItuU"
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={styles["App"]}>
+      <header className={styles["header"]}>
+        <img src={logo} className={styles["logo"]} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={styles["link"]}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <Button>Button</Button>
       </header>
     </div>
   );
